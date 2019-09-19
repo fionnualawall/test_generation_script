8351c4523908999acaeae9967ae153f06661dfd6
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-09-19 14:57:57 +0100

    Updated directory names and added some extra print statements

--- defects4jTest8SupportMOO.sh
+++ defects4jTest8SupportMOO.sh
@@ -12,31 +12,37 @@ do
     do
         for ii in {1..3}
         do
-            echo "$i $ii"
-            cd /home/ciaran/Desktop/defects4j-java-8-support/framework/bin/
-            /home/ciaran/Desktop/defects4j-java-8-support/framework/bin/run_evosuite.pl -p Lang -v "$a"b -n 1 -o /tmp/"$testingZone" -c "$i"
-            cd /tmp/"$testingZone"/Lang/evosuite-"$i"/1
+            echo "*** Starting processing for: $i $ii ***"
+            cd $TESTGEN/test-generation-defects4j/framework/bin
+            echo "*** Generating Test Suite ***"
+            $TESTGEN/test-generation-defects4j/framework/bin/run_evosuite.pl -p Lang -v "$a"b -n 1 -o $TESTGEN/"$testingZone" -c "$i"
+            echo "*** Test Suite Generated ***"
+            cd $TESTGEN/"$testingZone"/Lang/evosuite-"$i"/1
             dir_old=evosuite-"$i"
-            echo "$dir_old"
-            dir_name="${dir_old//:/}"; #mv "$dir" "$dir_name" && cd "$dir_name"
+            dir_name="${dir_old//:/}";
             cd ..
             cd ..
 
-                mkdir "$dir_name"
-                cd "$dir_name"
-                mkdir 1
-                cd /tmp/"$testingZone"/Lang/"$dir_old"/1
-                file_old=Lang-"$a"b-"$dir_old".1.tar.bz2
-                file_new=Lang-"$a"b-"$dir_name".1.tar.bz2
-                mv "$file_old" /tmp/"$testingZone"/Lang/"$dir_name"/1/
-                cd ..
-                rmdir 1
-                cd /tmp/"$testingZone"/Lang/"$dir_name"/1
-                mv "$file_old" "$file_new"
+            mkdir "$dir_name"
+            cd "$dir_name"
+            mkdir 1
+            cd $TESTGEN/"$testingZone"/Lang/"$dir_old"/1
+            file_old=Lang-"$a"b-"$dir_old".1.tar.bz2
+            file_new=Lang-"$a"b-"$dir_name".1.tar.bz2
+            mv "$file_old" $TESTGEN/"$testingZone"/Lang/"$dir_name"/1/
+            cd ..
+            rmdir 1
+            cd $TESTGEN/"$testingZone"/Lang/"$dir_name"/1
+            mv "$file_old" "$file_new"
 
-            /home/ciaran/Desktop/clean_java_8_defects4j/defects4j/framework/util/fix_test_suite.pl -p Lang -d . -v "$a"b
-            /home/ciaran/Desktop/clean_java_8_defects4j/defects4j/framework/bin/run_bug_detection.pl -p Lang -d . -o /Users/ciaranmurphy/Desktop/test8
-
+            echo "*** Fixing Test Suite ***"
+            $TESTGEN/test-generation-defects4j/framework/util/fix_test_suite.pl -p Lang -d . -v "$a"b
+            echo "*** Test Suite Fixed ***"
+			echo "*** Running Bug Detection ***"
+            $TESTGEN/test-generation-defects4j/framework/bin/run_bug_detection.pl -p Lang -d . -o $TESTGEN/d4j_output
+            echo "*** Bug Detection Run ***"
+			
+			echo "*** Cleaning Up Files ***"
             rm "$file_new"
             rm fix_test_suite.compile.log
             rm fix_test_suite.summary.log
@@ -47,6 +53,7 @@ do
             cd ..
             rmdir "$dir_old"
             rmdir "$dir_name"
+            echo "*** Finished processing for: $i $ii ***"
         done
     done
 done
